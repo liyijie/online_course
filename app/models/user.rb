81c860d4a9d68fc4bd9fa3c70a9babfd9cbea533
fanwei <fwshun@163.com>
2015-05-07 17:23:07 +0800

    add phone or username sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable#, :validatable
+
+  # Virtual attribute for authenticating by either username or phone
+  attr_accessor :login
+
+
+  def self.find_for_database_authentication(warden_conditions)
+   conditions = warden_conditions.dup
+   login = conditions.delete(:login)
+   where(conditions).where(["lower(username) = :value OR lower(phone) = :value", { :value => login.strip.downcase }]).first
+ end
 end
