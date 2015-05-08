2cb225397d806f0446a913a4b65b7bfc9f0f3705
fanwei <fwshun@163.com>
2015-05-08 12:45:41 +0800

    login page background image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :authenticate_user!  
 
   layout :get_layout
 
-  # 捕捉权限异常
-  rescue_from CanCan::AccessDenied do |e|
-    flash[:error] = I18n.t(:no_premission)
-    session = nil
-    redirect_to new_session_path(:user)
-  end
-
+  
 
   # layout全局设置
   #【引数】
