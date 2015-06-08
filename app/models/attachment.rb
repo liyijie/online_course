006e25e9330aa8b0241b89b2de39ec751f07b4e4
xiaocuixt <cjw624923@126.com>
2015-06-08 17:51:48 +0800

    fix upload bug

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -17,15 +17,27 @@
 class Attachment < ActiveRecord::Base
   belongs_to :attachmentable, polymorphic: true
 
-  has_attached_file :content, :styles => {
-    :c_860_500 => { :geometry => "860x500", :format => 'jpg', :time => 10 },
-    :c_260_140 => { :geometry => "260x140", :format => 'jpg', :time => 10 },
-    :thumb => { :geometry => "100x100", :format => 'jpg', :time => 10 },
-    :c_350_230 => { :geometry => "350x230", :format => 'jpg', :time => 10 },    #首页课程展示图
-    :c_500_350 => { :geometry => "500x350", :format => 'jpg', :time => 10 },
-    :c_420_285 => { :geometry => "420x285", :format => 'jpg', :time => 10 },    #课程展示页展示图
-    :c_340_200 => { :geometry => "340x200", :format => 'jpg', :time => 10 }     #教师展示页课程图片
-  }, :processors => [:ffmpeg]
+  has_attached_file :content, styles: lambda { |a| a.instance.check_file_type}
 
   validates_attachment :content, :content_type => {:content_type => %w()}
+
+  def check_file_type
+  	if is_video_type?
+  		{
+		    :c_860_500 => { :geometry => "860x500", :format => 'jpg', :time => 10, :processors => [:ffmpeg] },
+		    :c_260_140 => { :geometry => "260x140", :format => 'jpg', :time => 10, :processors => [:ffmpeg] },
+		    :thumb => { :geometry => "100x100", :format => 'jpg', :time => 10, :processors => [:ffmpeg] },
+		    :c_350_230 => { :geometry => "350x230", :format => 'jpg', :time => 10, :processors => [:ffmpeg] },    #首页课程展示图
+		    :c_500_350 => { :geometry => "500x350", :format => 'jpg', :time => 10, :processors => [:ffmpeg] },
+		    :c_420_285 => { :geometry => "420x285", :format => 'jpg', :time => 10, :processors => [:ffmpeg] },    #课程展示页展示图
+		    :c_340_200 => { :geometry => "340x200", :format => 'jpg', :time => 10, :processors => [:ffmpeg] }     #教师展示页课程图片
+	    }
+	   else
+	   	{}
+  	end
+  end
+
+  def is_video_type?
+    content_content_type =~ /^video/
+  end
 end
