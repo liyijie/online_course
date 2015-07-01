55b8f63dff4c73d367f3eded933c1ff33489cb18
fanwei <fwshun@163.com>
2015-07-01 16:06:26 +0800

    scope change

--- db/migrate/20150701063157_add_scope_to_course.rb
+++ db/migrate/20150701063157_add_scope_to_course.rb
@@ -1,5 +1,5 @@
 class AddScopeToCourse < ActiveRecord::Migration
   def change
-    add_column :courses, :scope, :integer, default: 1 #课程类别，1为校级，2为市级
+    add_column :courses, :scope, :integer, default: 1 #课程类别，1为市级，2为校级
   end
 end
