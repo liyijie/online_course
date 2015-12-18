fcf88f3247aa0335a4078438e3be164fcc342e34
xiaocuixt <cjw624923@126.com>
2015-12-18 17:51:53 +0800

    fix bug

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,14 +4,7 @@ module Admin
 	  # For APIs, you may want to use :null_session instead.
 	  protect_from_forgery with: :exception
 	  before_filter :authenticate_manager!
-    load_and_authorize_resource
+
 	  layout 'admin'
-
-	  private
-
-	  # 将会将CanCan通过current_manager获取当前登录用户
-    def current_ability
-      @current_ability ||= Ability.new(current_manager)
-    end
   end
 end
