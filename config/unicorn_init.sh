d70ca48eea258d33acfe1a30b22373d14fad617d
OnlyCZY <853852998@qq.com>
2016-02-26 16:20:26 +0800

    modify unicorn_init_sh

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/mnt/www/online_courses/online_course
+APP_ROOT=/www/deploy/online_course
 #换成自己项目的路径
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
