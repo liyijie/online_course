41118ddccd99176e69743c1771eb792859852d15
OnlyCZY <853852998@qq.com>
2016-03-07 13:52:21 +0800

    modify password for teachers in admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   #【注意】
   #【著作】2015/05/7 by fw
   def get_layout
-    if devise_controller? && params[:controller] != "users/registrations"
+    if devise_controller? && params[:controller] != "users/registrations" && params[:controller] != "devise/registrations"
       'login'
     elsif is_admin?
       'admin'
