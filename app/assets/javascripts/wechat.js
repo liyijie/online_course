52c60b61edecc1e861345e4869bccf2bf3b44ddd
fanwei <fanwei@netfarmer.com.cn>
2016-04-03 00:56:22 +0800

    js fix

--- app/assets/javascripts/wechat.js
+++ app/assets/javascripts/wechat.js
@@ -1,6 +1,3 @@
 //= require jquery
 //= require jquery_ujs
-//= require zepto.min
-//= require sui/sm.min
-//= require sui/sm-extend.min
 //= require wechat/common
--- app/assets/javascripts/wechat/common.coffee
+++
@@ -1,4 +0,0 @@
-$ ->
-  $.config = {
-    autoInit: true
-  }
