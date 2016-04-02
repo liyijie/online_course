843d2ec4c4316b4759c585f96aa4823de781186e
fanwei <fanwei@netfarmer.com.cn>
2016-04-02 22:42:55 +0800

    diymenu seed

--- db/seeds/diymenu.seeds.rb
+++ db/seeds/diymenu.seeds.rb
@@ -17,11 +17,11 @@ Diymenu.create!([{
 },{
   id: 8, parent_id: nil, name: '个人中心', key: '', url: '', is_show: true, sort: 2
 },{
-  id: 9, parent_id: 8, name: '修改密码', key: '', url: 'http://112.124.97.145/user', is_show: true, sort: 4
+  id: 9, parent_id: 8, name: '修改密码', key: '', url: 'http://112.124.97.145/wechat/users/password', is_show: true, sort: 4
 },{
-  id: 10, parent_id: 8, name: '个人资料', key: '', url: 'http://112.124.97.145/user', is_show: true, sort: 3
+  id: 10, parent_id: 8, name: '个人资料', key: '', url: 'http://112.124.97.145/wechat', is_show: true, sort: 3
 },{
-  id: 11, parent_id: 8, name: '我的问答', key: '', url: 'http://112.124.97.145/user/my_questions', is_show: true, sort: 2
+  id: 11, parent_id: 8, name: '我的问答', key: '', url: 'http://112.124.97.145/wechat/users/questions', is_show: true, sort: 2
 },{
-  id: 12, parent_id: 8, name: '我的测试', key: '', url: 'http://112.124.97.145/user/my_exams', is_show: true, sort: 1
+  id: 12, parent_id: 8, name: '我的测试', key: '', url: 'http://112.124.97.145/wechat/users/exams', is_show: true, sort: 1
 }])
