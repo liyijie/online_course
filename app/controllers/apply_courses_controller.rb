c41c3feedf3e6d61356772171ab700c17bcaf276
iboying <weboying@gmail.com>
2016-10-11 02:22:10 +0800

    fix

--- app/controllers/apply_courses_controller.rb
+++ app/controllers/apply_courses_controller.rb
@@ -1,8 +1,6 @@
 class ApplyCoursesController < ApplicationController
 	def index
-		@school_applied_courses = Course.undeleted.school_course.by_year
-		@city_applied_courses = Course.undeleted.city_course.by_year
-		@school_history_courses = Course.undeleted.school_course - @school_applied_courses
-		@city_history_courses = Course.undeleted.city_course - @city_applied_courses
+		@school_applied_courses = Course.undeleted.school_course
+		@city_applied_courses = Course.undeleted.city_course
 	end
 end
--- app/helpers/apply_courses_helper.rb
+++ app/helpers/apply_courses_helper.rb
@@ -1,2 +1,14 @@
 module ApplyCoursesHelper
+	# 判断课程申报日期
+	# true: >= 当前year
+	# false: < 当前year
+	def is_applying? applied_date_string
+		if applied_date_string.present?
+			applied_year = applied_date_string[0..3].to_i
+			now_year = Time.now.year.to_i
+			return applied_year >= now_year
+		else
+			return false
+		end
+	end
 end
